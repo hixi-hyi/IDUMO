b7862a7dd88c35cdce1f71f9cb415648dcb56b7d
Hiroyoshi Houchi <info@hixi-hyi.com>
2011-11-11 05:37:40 +0900

    commited test

---
+++ console/src/com/hixi_hyi/idumo/console/debug/SerialSendMain.java
@@ -0,0 +1,42 @@
+package com.hixi_hyi.idumo.console.debug;
+
+import com.hixi_hyi.idumo.console.*;
+import com.hixi_hyi.idumo.console.exec.*;
+import com.hixi_hyi.idumo.console.provider.*;
+import com.hixi_hyi.idumo.console.handler.*;
+import com.hixi_hyi.idumo.console.receiptor.*;
+import com.hixi_hyi.idumo.core.*;
+import com.hixi_hyi.idumo.core.exec.*;
+import com.hixi_hyi.idumo.core.provider.*;
+import com.hixi_hyi.idumo.core.handler.*;
+import com.hixi_hyi.idumo.core.receiptor.*;
+
+public class SerialSendMain extends AbstractConsoleMain {
+	@Override
+	public void init() {
+		setExecutionWithComponent(new SerialSendComponent());
+	}
+	
+	public static void main(String[] args) {
+		SerialSendMain main = new SerialSendMain();
+		main.exec();
+	}
+}
+
+class SerialSendComponent extends AbstractExecutionComponent {
+	@Override
+	public void onIdumoMakeFlowChart() throws IdumoException {
+		RandomByteProvider provider = new RandomByteProvider();
+		add(provider);
+		SerialSendReceiptor serial = new SerialSendReceiptor("/dev/stdout");
+		add(serial);
+		
+		connect(provider, serial);
+	}
+	
+	@Override
+	public void onIdumoPrepare() {
+		setLoopCount(-1);
+		setSleepTime(1000);
+	}
+}
---
+++ console/src/com/hixi_hyi/idumo/console/receiptor/SerialSendReceiptor.java
@@ -0,0 +1,91 @@
+package com.hixi_hyi.idumo.console.receiptor;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.hixi_hyi.idumo.core.ApplicationController;
+import com.hixi_hyi.idumo.core.IdumoException;
+import com.hixi_hyi.idumo.core.IdumoRunnable;
+import com.hixi_hyi.idumo.core.Receiver;
+import com.hixi_hyi.idumo.core.ReceiverWithOption;
+import com.hixi_hyi.idumo.core.Sender;
+import com.hixi_hyi.idumo.core.data.PipeData;
+import com.hixi_hyi.idumo.core.util.LogManager;
+
+public class SerialSendReceiptor implements IdumoRunnable, ReceiverWithOption, ApplicationController {
+	
+	private OutputStream	out;
+	private Sender			sender;
+	private String			serial;
+	
+	public SerialSendReceiptor(String serial) {
+		this.serial = serial;
+	}
+	
+	@Override
+	public void run() {
+		LogManager.log();
+		PipeData data = sender.getData();
+		byte[] bytedata = new byte[data.size()];
+		int i = 0;
+		LogManager.debug("size: " + data.size());
+		for (Object o : data) {
+			LogManager.debug(o.toString());
+			bytedata[i] = (Byte) o;
+			i++;
+		}
+		
+		try {
+			out.write(bytedata);
+			out.flush();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		
+	}
+	
+	@Override
+	public boolean isReady() {
+		return sender.isReady();
+	}
+	
+	@Override
+	public boolean setSender(Sender... senders) throws IdumoException {
+		if (senders.length != getInputSize()) {
+			return false;
+		}
+		for (Object o : senders[0].getDataType()) {
+			if (o != Byte.class) {
+				return false;
+			}
+		}
+		this.sender = senders[0];
+		return true;
+	}
+	
+	@Override
+	public int getInputSize() {
+		return 1;
+	}
+	
+	@Override
+	public void onIdumoStart() {
+		try {
+			out = new FileOutputStream(serial);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void onIdumoStop() {
+		try {
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
