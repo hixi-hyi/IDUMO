442fb1bff9c2d144306bcd23a533a89d26dbab87
Hiroyoshi Houchi <info@hixi-hyi.com>
2011-12-09 17:30:35 +0900

    add new provider structure

---
+++ android/src/com/hixi_hyi/idumo/android/provider/GPSProvider_Latitude.java
@@ -0,0 +1,86 @@
+package com.hixi_hyi.idumo.android.provider;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.LocationManager;
+
+import com.hixi_hyi.idumo.android.AndroidController;
+import com.hixi_hyi.idumo.android.sensor.GPSSensor;
+import com.hixi_hyi.idumo.core.IdumoException;
+import com.hixi_hyi.idumo.core.OptionMethodType;
+import com.hixi_hyi.idumo.core.Sender;
+import com.hixi_hyi.idumo.core.SenderWithOption;
+import com.hixi_hyi.idumo.core.data.PipeData;
+import com.hixi_hyi.idumo.core.util.LogManager;
+
+/**
+ * GPS情報を取得できるProvider
+ * 
+ * @author Hiroyoshi HOUCHI
+ * 
+ */
+public class GPSProvider_Latitude implements Sender, AndroidController {
+	
+	private GPSSensor	gps;
+	
+	public GPSProvider_Latitude(Activity activity) {
+		GPSSensor gpsSensor = GPSSensor.INSTANCE;
+		if (!gpsSensor.isInit()) {
+			LocationManager location = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+			gpsSensor.init(location);
+		}
+		this.gps = gpsSensor;
+	}
+	
+	@Override
+	public PipeData getData() {
+		LogManager.log();
+		if (!isReady()) {
+			return null;
+		}
+		
+		PipeData p = new PipeData();
+		p.add(gps.getLatitude());
+		return p;
+	}
+	
+	@Override
+	public boolean isReady() {
+		return gps.isReady();
+	}
+	
+	@Override
+	public void onIdumoDestroy() {}
+	
+	@Override
+	public void onIdumoPause() {
+		gps.unregister();
+	}
+	
+	@Override
+	public void onIdumoRestart() {}
+	
+	@Override
+	public void onIdumoResume() {
+		gps.register();
+	}
+	
+	@Override
+	public void onIdumoStart() {}
+	
+	@Override
+	public void onIdumoStop() {}
+	
+	@Override
+	public List<Class<?>> getDataType() throws IdumoException {
+		List<Class<?>> type = new ArrayList<Class<?>>();
+		type.add(Double.class);
+		return type;
+	}
+	
+}
