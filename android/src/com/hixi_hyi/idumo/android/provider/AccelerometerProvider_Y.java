3e5684fbc73d83ea11a4c744f242afbf4811f909
Hiroyoshi HOUCHI <info@hixi-hyi.com>
2011-12-10 01:52:08 +0900

    add new separate module

---
+++ android/src/com/hixi_hyi/idumo/android/provider/AccelerometerProvider_Y.java
@@ -0,0 +1,84 @@
+package com.hixi_hyi.idumo.android.provider;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.SensorManager;
+
+import com.hixi_hyi.idumo.android.AndroidController;
+import com.hixi_hyi.idumo.android.sensor.AccelerometerSensor;
+import com.hixi_hyi.idumo.core.IdumoRuntimeException;
+import com.hixi_hyi.idumo.core.OptionMethodType;
+import com.hixi_hyi.idumo.core.Sender;
+import com.hixi_hyi.idumo.core.SenderWithOption;
+import com.hixi_hyi.idumo.core.data.PipeData;
+import com.hixi_hyi.idumo.core.util.LogManager;
+
+/**
+ * Android上の加速度センサの値を提供するProvider
+ * 
+ * @author Hiroyoshi HOUCHI
+ * 
+ */
+public class AccelerometerProvider_Y implements Sender, AndroidController {
+	
+	private AccelerometerSensor	accel;
+	
+	public AccelerometerProvider_Y(Activity activity) {
+		AccelerometerSensor accelerometerSensor = AccelerometerSensor.INSTANCE;
+		if (!accelerometerSensor.isInit()) {
+			SensorManager sensor = (SensorManager) activity.getSystemService(Context.SENSOR_SERVICE);
+			accelerometerSensor.init(sensor);
+		}
+		this.accel = accelerometerSensor;
+	}
+	
+	@Override
+	public PipeData getData() {
+		LogManager.log();
+		PipeData p = new PipeData();
+		p.add(accel.getY());
+		return p;
+	}
+	
+	@Override
+	public List<Class<?>> getDataType() {
+		ArrayList<Class<?>> type = new ArrayList<Class<?>>();
+		type.add(Float.class);
+		return type;
+	}
+	
+
+	@Override
+	public boolean isReady() {
+		return accel.isReady();
+	}
+	
+	@Override
+	public void onIdumoDestroy() {}
+	
+	@Override
+	public void onIdumoPause() {
+		accel.unregister();
+	}
+	
+	@Override
+	public void onIdumoRestart() {}
+	
+	@Override
+	public void onIdumoResume() {
+		accel.register();
+	}
+	
+	@Override
+	public void onIdumoStart() {}
+	
+	@Override
+	public void onIdumoStop() {}
+	
+
+}
---
+++ android/src/com/hixi_hyi/idumo/android/provider/MagneticFieldProvider_Z.java
@@ -0,0 +1,83 @@
+package com.hixi_hyi.idumo.android.provider;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.SensorManager;
+
+import com.hixi_hyi.idumo.android.AndroidController;
+import com.hixi_hyi.idumo.android.sensor.MagneticFieldSensor;
+import com.hixi_hyi.idumo.core.IdumoException;
+import com.hixi_hyi.idumo.core.IdumoRuntimeException;
+import com.hixi_hyi.idumo.core.OptionMethodType;
+import com.hixi_hyi.idumo.core.Sender;
+import com.hixi_hyi.idumo.core.SenderWithOption;
+import com.hixi_hyi.idumo.core.data.PipeData;
+import com.hixi_hyi.idumo.core.util.LogManager;
+
+/**
+ * Android上の地磁気センサの情報を取得できるProvider
+ * 
+ * @author Hiroyoshi HOUCHI
+ * 
+ */
+public class MagneticFieldProvider_Z implements Sender, AndroidController {
+	
+	private MagneticFieldSensor	magnet;
+	
+	public MagneticFieldProvider_Z(Activity activity) {
+		MagneticFieldSensor magneticFieldSensor = MagneticFieldSensor.INSTANCE;
+		if (!magneticFieldSensor.isInit()) {
+			SensorManager sensor = (SensorManager) activity.getSystemService(Context.SENSOR_SERVICE);
+			magneticFieldSensor.init(sensor);
+		}
+		this.magnet = magneticFieldSensor;
+	}
+	
+	@Override
+	public PipeData getData() {
+		LogManager.log();
+		PipeData p = new PipeData();
+		p.add(magnet.getZ());
+		return p;
+	}
+	
+	@Override
+	public List<Class<?>> getDataType() {
+		ArrayList<Class<?>> type = new ArrayList<Class<?>>();
+		type.add(Float.class);
+		return type;
+	}
+	
+	@Override
+	public boolean isReady() {
+		return magnet.isReady();
+	}
+	
+	@Override
+	public void onIdumoDestroy() {}
+	
+	@Override
+	public void onIdumoPause() {
+		magnet.unregister();
+	}
+	
+	@Override
+	public void onIdumoRestart() {}
+	
+	@Override
+	public void onIdumoResume() {
+		magnet.register();
+	}
+	
+	@Override
+	public void onIdumoStart() {}
+	
+	@Override
+	public void onIdumoStop() {}
+	
+}
