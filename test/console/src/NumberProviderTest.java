7a9fa9957a3430ced769ddc79aee60b498cbf779
Hiroyoshi HOUCHI <info@hixi-hyi.com>
2012-04-26 01:11:17 +0900

    add : NumberProvider

---
+++ test/console/src/NumberProviderTest.java
@@ -0,0 +1,38 @@
+import com.hixi_hyi.idumo.common.provider.NumberProvider;
+import com.hixi_hyi.idumo.console.core.exec.IDUMOConsoleWrapper;
+import com.hixi_hyi.idumo.console.core.util.IDUMOConsoleLogger;
+import com.hixi_hyi.idumo.console.receiptor.ConsoleViewReceiptor;
+import com.hixi_hyi.idumo.core.exception.IDUMOException;
+import com.hixi_hyi.idumo.core.exec.IDUMOComponent;
+import com.hixi_hyi.idumo.core.util.IDUMOLogManager;
+
+public class NumberProviderTest extends IDUMOConsoleWrapper {
+	@Override
+	public void init() {
+		setExecutionWithComponent(new IDUMOComponent() {
+			@Override
+			public void onIdumoMakeFlowChart() throws IDUMOException {
+				NumberProvider idumo0 = new NumberProvider(10.0);
+				add(idumo0);
+				ConsoleViewReceiptor idumo1 = new ConsoleViewReceiptor();
+				add(idumo1);
+
+				connect(idumo0, idumo1);
+
+			}
+
+			@Override
+			public void onIdumoPrepare() {
+				setLoopCount(1);
+				setSleepTime(1000);
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		IDUMOLogManager.DEBUG = true;
+		IDUMOLogManager.LOGGER = new IDUMOConsoleLogger();
+		NumberProviderTest main = new NumberProviderTest();
+		main.exec();
+	}
+}
